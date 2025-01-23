698ab8165383dd01d0fa0331b14957be26e6392e
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-23 16:24:47 +0000

    initial setup

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,14 @@
-# Write your soltuion here!
+# Write your solution here!
+require "http"
+require "dotenv/load"
+
+pp "Hi! Where are you located?"
+# user_loc = gets.chomp # comment in/out to facilitate code testing
+user_loc = "151 N Michigan Ave Chicago, IL 60601" # hardcoded location for development
+pp user_loc
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{ENV.fetch("GMAPS_KEY")}"
+
+pp gmaps_url
+
+pp HTTP.get(gmaps_url)
