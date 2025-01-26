205423c1f8c3a49f1bc2ae88f29c861a5e28187e
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-26 19:20:08 +0000

    Basic forecast done

--- umbrella.rb
+++ umbrella.rb
@@ -3,10 +3,10 @@ require "http"
 require "json"
 require "dotenv/load"
 
-pp "Hi! Where are you located?"
+puts "Hi! Where are you located?"
 # user_loc = gets.chomp # comment in/out to facilitate code testing
 user_loc = "JP Morgan Chase Tower" # hardcoded location for development
-pp "Checking the weather at #{user_loc}..."
+puts "Checking the weather at #{user_loc}..."
 
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{ENV.fetch("GMAPS_KEY")}"
 
@@ -17,7 +17,7 @@ gmaps_hash = JSON.parse(HTTP.get(gmaps_url))
 gmaps_coordinates = gmaps_hash.fetch("results")[0].fetch("geometry").fetch("location")
 lat = gmaps_coordinates.fetch("lat")
 lng = gmaps_coordinates.fetch("lng")
-pp "Your coordinates are: lat = #{lat.round(3)}; lng = #{lng.round(3)}. Analyzing..."
+puts "Your coordinates are: lat = #{lat.round(3)}; lng = #{lng.round(3)}. Analyzing..."
 
 
 # pull and store weather info from Pirate Weather
@@ -29,13 +29,26 @@ current_weather_hash = pirateweather_hash.fetch("currently")
 current_weather_summary = current_weather_hash.fetch("summary")
 current_temp = current_weather_hash.fetch("temperature")
 
-pp "It is currently #{current_temp}\u00B0F."
-pp "Current conditions: #{current_weather_summary}"
+puts "It is currently #{current_temp}\u00B0F."
+puts "Current conditions: #{current_weather_summary}"
 
 # hourly forecast
 hourly_fcst_hash = pirateweather_hash.fetch("hourly").fetch("data")
 next_twelve_hash = hourly_fcst_hash[1..12]
+
+need_umbrella = false
+next_hour = 0
+
+puts "Next 12-hour precipation likelihood:"
 next_twelve_hash.each do |hour|
-  prob_rain = hour.fetch{"precipProbability"}
-  pp prob_rain
+  next_hour = next_hour + 1
+  prob_rain = hour.fetch("precipProbability")
+  if prob_rain >= 0.1
+    need_umbrella = true
+  end
+  puts "   - In #{next_hour} hour(s), #{(prob_rain*100).round(0)}% chance"
+end
+
+if need_umbrella
+  puts "You might want to carry an umbrella!"
 end
