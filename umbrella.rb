5ff44ef52e59e20eecd46c9858f525c87130658c
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-26 18:44:28 +0000

    basic weather

--- umbrella.rb
+++ umbrella.rb
@@ -5,8 +5,8 @@ require "dotenv/load"
 
 pp "Hi! Where are you located?"
 # user_loc = gets.chomp # comment in/out to facilitate code testing
-user_loc = "151 N Michigan Ave Chicago, IL 60601" # hardcoded location for development
-pp user_loc
+user_loc = "JP Morgan Chase Tower" # hardcoded location for development
+pp "Checking the weather at #{user_loc}..."
 
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{ENV.fetch("GMAPS_KEY")}"
 
@@ -17,8 +17,17 @@ gmaps_hash = JSON.parse(HTTP.get(gmaps_url))
 gmaps_coordinates = gmaps_hash.fetch("results")[0].fetch("geometry").fetch("location")
 lat = gmaps_coordinates.fetch("lat")
 lng = gmaps_coordinates.fetch("lng")
+pp "Your coordinates are: lat = #{lat.round(3)}; lng = #{lng.round(3)}. Analyzing..."
+
 
 # pull and store weather info from Pirate Weather
-pirateweather_url = "https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/#{lat},#{lng}"
+pirateweather_url = "https://api.pirateweather.net/forecast/#{ENV.fetch("PIRATE_WEATHER_KEY")}/#{lat},#{lng}"
 pirateweather_hash = JSON.parse(HTTP.get(pirateweather_url))
-pp pirateweather_hash
+current_weather_hash = pirateweather_hash.fetch("currently")
+
+# current weather info
+current_weather_summary = current_weather_hash.fetch("summary")
+current_temp = current_weather_hash.fetch("temperature")
+
+pp "It is currently #{current_temp}\u00B0F."
+pp "Current conditions: #{current_weather_summary}"
