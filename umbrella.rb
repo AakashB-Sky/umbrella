fe7a839311a1df6fe60a0248cbf276ec0d626a6e
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-26 18:10:59 +0000

    pulled coordinates from gmaps api

--- umbrella.rb
+++ umbrella.rb
@@ -10,6 +10,12 @@ pp user_loc
 
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{ENV.fetch("GMAPS_KEY")}"
 
-pp gmaps_url
+# pull and store info from Google Maps API
+gmaps_hash = JSON.parse(HTTP.get(gmaps_url))
 
-pp HTTP.get(gmaps_url)
+# pp gmaps_hash # print the gmaps_hash
+gmaps_coordinates = gmaps_hash.fetch("results")[0].fetch("geometry").fetch("location")
+# pp gmaps_coordinates
+lat = gmaps_coordinates.fetch("lat")
+lng = gmaps_coordinates.fetch("lng")
+pp "lat = #{lat}; lng = #{lng}"
